607868917ef6367ae42c98f8e400fe85b42395b6
214145047@qq.com <214145047@qq.com>
2019-09-14 19:35:28 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+###. Initialization of the vector with a default value is important because without a default value, 
+### data <- x$get() generates the following error message.
+# makeVector <- function(x = numeric()) {
+#   m <- NULL
+#   set <- function(y) {
+#     x <<- y
+#     m <<- NULL
+#   }
+#   get <- function() x
+#   setmean <- function(mean) m <<- mean
+#   getmean <- function() m
+#   list(set = set, get = get,
+#        setmean = setmean,
+#        getmean = getmean)
+# }
+# 
+# cachemean <- function(x, ...) {
+#   m <- x$getmean()
+#   if(!is.null(m)) {
+#     message("getting cached data")
+#     return(m)
+#   }
+#   data <- x$get()
+#   m <- mean(data, ...)
+#   x$setmean(m)
+#   m
+# }
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
